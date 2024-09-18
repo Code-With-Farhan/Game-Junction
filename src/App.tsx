a8bf6c2ca3ddb87c45734d82fb1172fa1eb76649
rockstarsmaf <79184468+rockstarsmaf@users.noreply.github.com>
2024-09-19 02:07:51 +0400

    Game-Junction

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
+import { Genre } from "./hooks/useGenres";
+import { useState } from "react";
+import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/usePlatform";
+import SortSelector from "./components/SortSelector";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export interface GameQuery {
+  selectedGenre: Genre | null;
+  selectedPlatform: Platform | null;
+  selectedSort: string | null;
+  searchText: string;
 }
 
-export default App
+function App() {
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+  // const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  // const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+  //   null
+  // );
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //1024px
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        ></NavBar>
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          <GenreList
+            selectedGenre={gameQuery?.selectedGenre?.id || null}
+            onSelectGenre={(selectedGenre) =>
+              setGameQuery({ ...gameQuery, selectedGenre })
+            }
+          />
+        </GridItem>
+      </Show>
+      <GridItem area="main">
+        <HStack marginLeft="40px">
+          <PlatformSelector
+            selectedPlatform={gameQuery?.selectedPlatform || null}
+            onSelectPlatform={(selectedPlatform) =>
+              setGameQuery({ ...gameQuery, selectedPlatform })
+            }
+          />
+          <SortSelector
+            selectedSort={gameQuery?.selectedSort || null}
+            onSelectSort={(selectedSort) =>
+              setGameQuery({ ...gameQuery, selectedSort })
+            }
+          />
+        </HStack>
+        <GameGrid gameQuery={gameQuery} />
+      </GridItem>
+      {/* <GridItem area="nav">Nav</GridItem> */}
+    </Grid>
+  );
+}
+
+export default App;

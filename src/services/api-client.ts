607b76ff7331f1e7d3ef451f32eba45013e4bbba
rockstarsmaf <79184468+rockstarsmaf@users.noreply.github.com>
2024-09-19 03:24:01 +0400

    used env file for key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "2bf4ac228ada4ba8a17f419740b39a6c",
+    key: import.meta.env.VITE_RAWG_KEY,
   },
 });
+console.log("API URL:", import.meta.env.VITE_RAWG_KEY);
